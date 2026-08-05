0e9dcbe7c307bc887e1066bec4bbefa36cb81be1
PrasanthChandaka <prasanthchandaka111a@gmail.com>
2026-08-05 16:24:12 +0530

    sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
 const App = () => {
-  return (
-    <div>
-      <div>
-        Hii there I am Prasanth Chandaka
-        <h1>hi this is git </h1>
-      </div>
-
-      <div>
-        <p>i am manoj</p>
-      </div>
-      <div>
-        My name is Prasanth,I am an aspiring web dev and Devops Engineer as
-        well.
-      </div>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
